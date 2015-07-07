b25ba5e8acd8b886c8392e0d08c129f0b25a1fd5
James Gordon <gordonje@gmail.com>
2015-07-07 13:29:05 -0500

    added csv output

--- openstates_api/test_api.py
+++ openstates_api/test_api.py
@@ -1,7 +1,7 @@
 
 from config import key
 import requests
-import json
+import csv
 
 os_bill_id = 'MOB00003794'
 
@@ -9,10 +9,18 @@ url = 'http://openstates.org/api/v1/bills/{0}/?apikey={1}'.format(os_bill_id, ke
 
 response = requests.get(url)
 
-json_results = response.json()
+results = response.json()
 
-for action in json_results['actions']:
-	print action
+with open('results.csv', 'wb') as csv_out:
 
-with open('results.json', 'wb') as out_file:
-	out_file.write(response.content)
+	fields = list(results['actions'][0].viewkeys())
+	writer = csv.DictWriter(csv_out, fieldnames = fields)
+
+	writer.writeheader()
+
+	for action in results['actions']:
+		print action
+		writer.writerow(action)
+
+with open('results.json', 'wb') as json_out:
+	json_out.write(response.content)
