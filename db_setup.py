256078045cf62708a42b1b7248f1521781ff8982
James Gordon <gordonje@gmail.com>
2015-07-20 09:15:03 -0500

    Added new models and look-ups

--- db_setup.py
+++ db_setup.py
@@ -6,5 +6,48 @@ from urlparse import urlparse
 
 db.create_tables([
 				  Source_Page
-				, Lawmaker
+				, Assembly
+				, Chamber
+				, Legislator_Assembly
+				, Session_Type
+				, Session
+				, Bill_Type
+				, Bill
 			], True)
+
+
+with open('look_ups/session_types.csv', 'rU', ) as f:
+	reader = csv.DictReader(f)
+
+	for row in reader:
+		try:
+			Session_Type.create(**row)
+		except IntegrityError:
+			print '{} is already inserted.'.format(row['name'])
+
+with open('look_ups/assemblies.csv', 'rU', ) as f:
+	reader = csv.DictReader(f)
+
+	for row in reader:
+		try:
+			Assembly.create(**row)
+		except IntegrityError:
+			print '{} is already inserted.'.format(row['name'])
+
+with open('look_ups/chambers.csv', 'rU', ) as f:
+	reader = csv.DictReader(f)
+
+	for row in reader:
+		try:
+			Chamber.create(**row)
+		except IntegrityError:
+			print '{} is already inserted.'.format(row['name'])
+
+with open('look_ups/bill_types.csv', 'rU', ) as f:
+	reader = csv.DictReader(f)
+
+	for row in reader:
+		try:
+			Bill_Type.create(**row)
+		except IntegrityError:
+			print '{} is already inserted.'.format(row['name'])
--- models.py
+++ models.py
@@ -30,6 +30,7 @@ class BaseModel(Model):
 	class Meta:
 		database = db
 
+
 class Source_Page(BaseModel):
 	url = CharField()
 	scheme = CharField()
@@ -50,24 +51,126 @@ class Source_Page(BaseModel):
 			(('url', 'parent_id'), True),
 		)
 
-class Lawmaker(BaseModel):
-	first = CharField()
-	middle = CharField(null = True)
-	last = CharField()
-	chamber = CharField()
-	year = IntegerField()
+
+class Assembly(BaseModel):
+	number = IntegerField(primary_key = True)
+	name = CharField()
+	created_date = DateTimeField(default = datetime.now)
+
+
+class Chamber(BaseModel):
+	id = CharField(primary_key = True)
+	name = CharField()
+	title = CharField()
+	full_title = CharField()
+	past_sessions_url = CharField()
+	created_date = DateTimeField(default = datetime.now)
+
+
+class Legislator_Assembly(BaseModel):
+	person = ForeignKeyField(Person, related_name = 'terms')
+	assembly = ForeignKeyField(Assembly, related_name = 'members')
+	chamber = ForeignKeyField()
 	party = CharField()
 	district = CharField()
-	phone = CharField(null = True)
-	room = CharField(null = True)
-	fax = CharField(null = True)
-	email = CharField(null = True)
-	source_page = ForeignKeyField(Source_Page)
+	counties = CharField()
+	created_date = DateTimeField(default = datetime.now)
+
+	class Meta:
+        primary_key = CompositeKey('person', 'assembly')
+
+
+class Session_Type(BaseModel):
+	id = CharField(primary_key = True)
+	name = CharField(unique = True)
+	created_date = DateTimeField(default = datetime.now)
+
+
+class Session(BaseModel):
+	assembly = ForeignKeyField(Assembly, related_name = 'sessions')
+	year = IntegerField()
+	session_type = ForeignKeyField(Session_Type)
+	name = get_name()
+	created_date = DateTimeField(default = datetime.now)
+
+	def get_name(self):
+		return '{0} {1}'.format(self.year, self.session_type.name)
+
+
+class Person(BaseModel):
+	first_name = CharField()
+	middle_name = CharField()
+	last_name = CharField()
+	created_date = DateTimeField(default = datetime.now)
 
 	class Meta:
 		indexes = (
-			(('year', 'district'), True),
+			(('first', 'middle', 'last'), True),
 		)
+
+
+class Bill_Type(BaseModel):
+	id = CharField(primary_key = True)
+	description = CharField()
+	chamber = ForeignKeyField(Chamber, related_name = 'bill_types')
+	created_date = DateTimeField(default = datetime.now)
+
+
+class Bill(BaseModel):
+	session = ForeignKeyField(Session, related_name = 'bills')
+	bill_type = ForeignKeyField(Bill_Type)
+	number = IntegerField()
+	title = CharField(null = True)
+	description = CharField()
+	lr_number = CharField(null = True)
+	sponsor = ForeignKeyField(Legislator_Assembly, related_name = 'sponsored_bills')
+	committee = ForeignKeyField(Committee, null = True	)
+	effective_date = DateField(null = True)
+	EffDate	BillCombinedWith
+	created_date = DateTimeField(default = datetime.now)
+
+	class Meta:
+		indexes = (
+			(('session', 'bill_type', 'number'), True),
+		)
+
+	# 	# does the same bill show up in multiple sessions?
+	# 	bill_type = ForeignKeyField(Bill_Type)
+	# 	number = IntegerField
+	# 	brief_desc = TextField()
+
+	# 	sponsor_first_name,
+	# 	sponsor_last_name,
+	# 	sponsor_district,
+	# 	lr_number,
+	# 	effective_date,
+	# 	last_action_date,
+	# 	last_action_desc,
+	# 	next_hearing,
+	# 	calendar
+
+
+# class Lawmaker(BaseModel):
+# 	first = CharField()
+# 	middle = CharField(null = True)
+# 	last = CharField()
+# 	chamber = CharField()
+# 	year = IntegerField()
+# 	party = CharField()
+# 	district = CharField()
+# 	phone = CharField(null = True)
+# 	room = CharField(null = True)
+# 	fax = CharField(null = True)
+# 	email = CharField(null = True)
+# 	source_page = ForeignKeyField(Source_Page)
+# 	created_date = DateTimeField(default = datetime.now)
+
+# 	class Meta:
+# 		indexes = (
+# 			(('year', 'district'), True),
+		)
+
+
 
 	# def full_url():
 	# # concatenates url parts, returns a full url
@@ -91,39 +194,6 @@ class Lawmaker(BaseModel):
 # 	abbr_title = CharField()
 # 	url = CharField()
 # 	created_date = DateTimeField(default = datetime.now)
-
-# class Bill_Type(BaseModel):
-# 	short = CharField(primary_key = True)
-# 	full = CharField()
-# 	chamber = CharField()
-# 	created_date = DateTimeField(default = datetime.now)
-
-# 	# does the same bill show up in multiple sessions?
-# class Bill(BaseModel):
-# 	bill_type = ForeignKeyField(Bill_Type)
-# 	number = IntegerField
-# 	brief_desc = TextField()
-# 	sponsor = ForeignKeyField(Legislator_term_session)
-
-# 	# add methods for showing url_id and display_name
-# 	sponsor_first_name,
-# 	sponsor_last_name,
-# 	sponsor_district,
-# 	lr_number,
-# 	effective_date,
-# 	last_action_date,
-# 	last_action_desc,
-# 	next_hearing,
-# 	calendar
-
-# class Legislator(BaseModel):
-
-# 	state = CharField(primary_key = True)
-# 	num_dams = IntegerField()
-# 	created_date = DateTimeField(default = datetime.now)
-
-# 	class Meta:
-# 		order_by = ('state',)
 
 
 
