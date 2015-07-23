e24a79a1b49c2ba257a77ada32d8318c23e7c3c6
James Gordon <gordonje@gmail.com>
2015-07-23 15:45:38 -0500

    Now creating sessions, if necessary

--- get_past_session_urls.py
+++ get_past_session_urls.py
@@ -1,11 +1,6 @@
 import os
 from utils import *
 
-cham_urls = [
-	  'http://www.house.mo.gov/sitemap.aspx?pid=24'
-	, 'http://www.senate.mo.gov/pastsessions.htm'
-]
-
 if not os.path.exists('past_content/'):
 	os.makedirs('past_content/')
 
@@ -17,11 +12,11 @@ if not os.path.exists('past_content/S/'):
 
 with session() as r_sesh:
 
-	for cham_url in cham_urls:
+	for chamber in Chamber.select():
 
-		response = r_sesh.get(cham_url)
+		response = r_sesh.get(chamber.past_sessions_url)
 
-		for link in extract_links(response.content, cham_url):
+		for link in extract_links(response.content, chamber.past_sessions_url):
 			
 			link['year'] = re.search("\d{4}", link['name']).group()
 			link['name'] = re.sub("\d{4}", "", link['name']).lstrip(' - ').strip()
@@ -33,9 +28,40 @@ with session() as r_sesh:
 			except IntegrityError:
 				pass
 				
-	# collect the urls from within each session
+	# go through each session
 
-	for sess_page in Source_Page.select().where(Source_Page.parent_id == 0).order_by(Source_Page.chamber, Source_Page.year.desc()):
+	for sess_page in Source_Page.select(
+							   ).where(Source_Page.parent_id == 0 
+							   ).order_by(
+							   		Source_Page.chamber
+							   	  , Source_Page.year.desc()
+							   ):
+
+		# create a new session, if necessary
+
+		# going to use the senate as the authoritative list of sessions
+		if sess_page.chamber == 'S':
+		# even numbered years are the second of assemblies, odd years are the first of assemblies
+
+			if 'Extraordinary' in sess_page.name:
+				session_type = 'E'
+			else:
+				session_type = 'R'
+
+			if sess_page.year % 2 == 0:
+				assembly = Assembly.get(Assembly.end_year == sess_page.year)
+			else:
+				assembly = Assembly.get(Assembly.start_year == sess_page.year)
+
+			Session.get_or_create(
+				  assembly = assembly
+				, year = sess_page.year
+				, session_type = session_type
+				, name = sess_page.name
+			)
+
+		# now collect all the urls within the session page
+
 		print 'Getting urls for {0} {1} {2}...'.format(sess_page.chamber, sess_page.year, sess_page.name)
 
 		directory = 'past_content/{0}/{1}_{2}/'.format(sess_page.chamber, sess_page.year, sess_page.name.replace(' ', '_'))
@@ -65,5 +91,11 @@ with session() as r_sesh:
 						Source_Page.create(**link)
 				except IntegrityError:
 					pass
+
+		# there's an additional 2001 extraordinary session that the senate is missing
+		# house page is http://house.mo.gov/billtracking/spec01/billist.htm
+		# senate page is http://www.senate.mo.gov/01info/sp/bil-list.htm
+
+		Session.get_or_create(assembly = 91, session_type = 'E', name = '2001 Extraordinary Session', year = 2001)
 
 print 'fin.'
