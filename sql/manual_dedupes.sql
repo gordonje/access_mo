0538799f1342de08d8600ed0b6e879b663b35331
James Gordon <gordon.je@gmail.com>
2015-10-27 11:53:53 -0500

    Fixed issues related to missing special election

--- sql/manual_dedupes.sql
+++ sql/manual_dedupes.sql
@@ -1,4 +1,5 @@
 -- Dedupe Linda Black Fischer's record
+-- http://house.mo.gov/member.aspx?year=2014&district=117
 UPDATE race_candidate
 SET person_id = 220
 WHERE person_id = 2081;
@@ -12,6 +13,7 @@ FROM person
 WHERE id NOT IN (SELECT person_id FROM person_name);
 
 -- Add Lana Ladd Baker's record
+-- http://house.mo.gov/content.aspx?info=/bills02/member02/bio076.htm
 UPDATE person
 SET middle_name = '', last_name = 'Ladd Baker'
 WHERE middle_name = 'Ladd';
